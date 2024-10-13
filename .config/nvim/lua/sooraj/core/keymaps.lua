fb73ac1a64a5d1c84f2daaacf5b0f1efa9e10d2e
sooraj <soorajbhooshan86@gmail.com>
2024-10-13 12:37:33 +0530

    ADD: basic options and keymaps added

--- .config/nvim/lua/sooraj/core/keymaps.lua
+++ .config/nvim/lua/sooraj/core/keymaps.lua
@@ -3,4 +3,19 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+-- delete single character without copying into register
+keymap.set("n", "x", '"_x')
+
 keymap.set("i","jk","<ESC>",{desc = "Exit insert mode"})
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+keymap.set("n", "<leader>vs", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>hs", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>es", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>xs", "<cmd>close<CR>", { desc = "Close current split" })
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
--- .config/nvim/lua/sooraj/core/options.lua
+++ .config/nvim/lua/sooraj/core/options.lua
@@ -1,4 +1,23 @@
 
 
 local opt = vim.opt
+																
 opt.relativenumber = true
+opt.clipboard:append("unnamedplus")
+opt.number = true
+opt.cursorline = true
+opt.background = "dark"
+opt.scrolloff = 8
+opt.ignorecase = true
+opt.smartcase = true
+
+-- opt.tabstop = 1
+-- opt.shiftwidth = 2
+-- opt.expandtab = true
+-- opt.autoindent = true
+-- opt.wrap = false
+-- opt.termguicolors = true
+-- opt.signcolumn = "yes"
+-- opt.backspace = "indent,eol,start"
+-- opt.splitright = true
+-- opt.splitbelow = true
